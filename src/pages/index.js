e3bca5d90e6eefa0098c19efbab51e6a1b7cd7de
Laura-Moya <l.moya@alumnos.upm.es>
2023-07-27 20:37:23 +0200

    Home creo

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Home from './Home';
+import About from './About';
+import Proyectos from './Proyectos';
+import Contacto from './Contacto';
+
+export {
+    Home, 
+    About,
+    Proyectos,
+    Contacto
+}
