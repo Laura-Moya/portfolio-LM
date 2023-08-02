d5e0cf5ceb25391f0beacd6f0f0040a27480de6b
Laura-Moya <l.moya@alumnos.upm.es>
2023-08-02 18:19:29 +0200

    Conseguido centrar los dos botones inferiores de la Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,13 +8,6 @@ import fondo_home from '../images/Laura_1_70.png';
 const Home = () => {
   return (
     <Contenedor_Home >
-      {/*<div className="container" style={{float: 'left'}}>
-        <img src={fondo_home} className='fondo_home'></img>
-        <div class="center">
-        <p>The text in this div is centered horizontally.</p>
-      </div>
-      </div>*/}
-
       <img class="bg" src={fondo_home}></img>
       
       <h1>PORTFOLIO</h1>
@@ -22,6 +15,10 @@ const Home = () => {
         <p class='titulo'>Laura<br></br>Moya</p>
       </div>
       
+      <div className='bottom'>
+        <p className='bottom-labels'>DISEÑADORA Y DESARROLLADORA WEB</p>
+        <p className='bottom-labels'>ESCRIBEME</p>
+      </div>
       
     </Contenedor_Home>
   )
@@ -47,13 +44,9 @@ h1 {
 .titulo {
   font-family: 'Bogart', sans-serif;
   font-size: 5.5rem;
-  /*line-height: 7rem;*/
 }
 
 .centrado-pantalla {
-  /*background-color: #aa21ff;
-  width: 300px;
-  min-height: 300px;*/
   display: flex;
   justify-content: center;
   align-items: center;
@@ -63,6 +56,20 @@ h1 {
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+}
+
+.bottom {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  position: absolute;
+  bottom: 9rem;
+  width: 100%;
+}
+
+.bottom-labels {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 1rem;
+  font-weight: 900 !important;
 }
 `
 
